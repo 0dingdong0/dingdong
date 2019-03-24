6ce77d8374385e80a80c0825b78415ed312f2ad3
odingdingo <odingdongo@hotmail.com>
2019-03-25 07:55:11 +0800

    refactoring models:   * base.article

--- base/models/article.py
+++ base/models/article.py
@@ -6,8 +6,15 @@ from .category import Category
 
 class Article(models.Model):
 
-    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, null=True, blank=True)
+    supplier = models.ForeignKey(
+        Supplier, on_delete=models.CASCADE,
+        related_name="suppliers",
+        related_query_name="supplier")
+    category = models.ForeignKey(
+        Category,
+        null=True, blank=True,
+        related_name="articles",
+        related_query_name="article")
 
     code = models.CharField("编号", max_length="10")
     name = models.CharField("名字", max_length=20)
