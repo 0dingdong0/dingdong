8688360744ce55e3a770f844e4d5f86fc0a55b32
odingdongo <odingdongo@hotmail.com>
2019-03-26 10:29:42 +0800

    setup django rest framework

---
+++ base/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include, re_path
+
+
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+    # re_path(r'^api-auth/', include('rest_framework.urls')),
+]
--- dingdong/urls.py
+++ dingdong/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls')),
+    re_path(r'^api-auth/', include('rest_framework.urls')),
 ]
