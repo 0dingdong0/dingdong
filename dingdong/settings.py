2dd5913ebb984aa7e989760e27fa92c62655daaa
odingdingo <odingdongo@hotmail.com>
2019-04-24 22:53:00 +0800

    setup jupyter notebook for django

--- dingdong/settings.py
+++ dingdong/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'django_extensions',
     'base.apps.BaseConfig',
     'inventory.apps.InventoryConfig',
 ]
@@ -153,8 +154,8 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
 
     'DEFAULT_PERMISSION_CLASSES': [
-        # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
         'rest_framework.permissions.IsAuthenticated'
+        'rest_framework.permissions.DjangoModelPermissions'
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
