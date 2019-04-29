3cef6dbbe756fa82d9b9a9cbc2d6431c0a046f89
odingdingo <odingdongo@hotmail.com>
2019-04-30 06:48:14 +0800

    2) change lifetime of jwt token for test purpose

--- dingdong/settings.py
+++ dingdong/settings.py
@@ -165,8 +165,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=1),
     # 'ROTATE_REFRESH_TOKENS': False,
     # 'BLACKLIST_AFTER_ROTATION': True,
     #
