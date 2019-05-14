6941c144a011e59c866e1e26f89d06fbcaefe546
odingdingo <odingdongo@hotmail.com>
2019-05-14 23:00:01 +0800

    implement action 'set_login_name'

--- base/api/views/account.py
+++ base/api/views/account.py
@@ -4,7 +4,7 @@ from base.api.serializers import AccountSerializer
 from rest_framework.decorators import action
 from rest_framework import viewsets
 from rest_framework.response import Response
-
+from django.db.utils import IntegrityError
 
 class AccountViewSet(viewsets.ModelViewSet):
     """
@@ -29,13 +29,13 @@ class AccountViewSet(viewsets.ModelViewSet):
     @action(detail=True, methods=['patch'])
     def set_password(self, request, pk=None):
         user = self.get_object()
-        print('-----------------------------')
-        print(request.user.id, user.id)
-        if (not request.user.is_superuser) or (request.user.id == user.id):
+
+        # @TODO: add object level permission checking
+        if request.user.id == user.id:
             if not user.check_password(request.data['password']):
                 return Response({
                     'password': ['密码不正确']
-                }, status.HTTP_400_BAD_REQUEST)
+                }, status.HTTP_405_METHOD_NOT_ALLOWED)
 
         new_password = request.data['new_password']
         confirm_password = request.data['confirm_password']
@@ -48,4 +48,29 @@ class AccountViewSet(viewsets.ModelViewSet):
         user.set_password(new_password)
         user.save()
         return Response(None, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=['patch'])
+    def set_login_name(self, request, pk=None):
+        user = self.get_object()
+
+        # @TODO: add object level permission checking
+        if request.user.id == user.id:
+            if not user.check_password(request.data['password']):
+                return Response({
+                    'password': ['密码不正确']
+                }, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        user.username = request.data['username']
+
+        try:
+            user.save()
+            return Response(
+                {'username': user.username},
+                status=status.HTTP_200_OK)
+        except IntegrityError:
+            return Response(
+                {'username': request.data['username']},
+                status=status.HTTP_409_CONFLICT)
+
+
 
